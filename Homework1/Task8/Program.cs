b8be9ef1d82c10f059bb0d3bfd812f5bd4205911
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-04 21:20:10 +0300

    Решил задачу

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+
+int a = int.Parse(ReadLine()!);
+int n = 1;
+
+while (n <= a)
+{
+   if (n % 2 == 0)
+   {
+     Write($"{n} ");
+     n = n + 1;
+   }
+    else
+    {
+    n = n + 1;
+    }
+}
