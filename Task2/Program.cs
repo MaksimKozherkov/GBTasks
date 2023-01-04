31f2947f46edf8419811d9cd8e649dcc0252d965
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-04 18:30:14 +0300

    Решили 2 задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите номер дня недели: ");
+int day = int.Parse(ReadLine()!);
+
+if (day <= 7 && day >= 1)
+{
+    if (day == 1)
+    {
+        Write("Понедельник");
+    }
+    if (day == 2)
+    {
+        Write("Вторник");
+    }
+    if (day == 3)
+    {
+        Write("Среда");
+    }
+    if (day == 4)
+    {
+        Write("Четверг");
+    }
+    if (day == 5)
+    {
+        Write("Пятница");
+    }
+    if (day == 6)
+    {
+        Write("Суббота");
+    }
+    if (day == 7)
+    {
+        Write("Воскресенье");
+    }
+
+}
+else
+{
+     Write("В неделе всего 7 дней");
+}
