46271dc42a9b855d2054f46630baacd9b88d9e03
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-04 20:38:56 +0300

    Решил задачу

--- Homework1/Task6/Program.cs
+++ Homework1/Task6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Является ли число четным?");
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a % 2 == 0)
+{
+    Write("Четное");
+}
+else
+{
+Write("Не четное");
+}
