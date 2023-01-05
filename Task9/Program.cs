97690431e6a20962fe0c3d3a68e8220b80e47aed
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-05 21:13:59 +0300

    Террарный оператор

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+int number = new Random().Next(10, 100);
+WriteLine(number);
+WriteLine(number%2==0?"Четное":"Нечетное");
+WriteLine(number/10>number%10?number / 10:number % 10); //Тернарный оператор 1 число после ? при выполнении условия число после : при невыполнении
+
+// if(number/10>number%10)
+// {
+//     WriteLine(a1);
+// }
+// else
+// {
+//     WriteLine(a2);
+// }
