4403ac0ce45dedbb61fcd437cfcf28b7b50cea4b
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-06 00:17:42 +0300

    Решил

---
+++ Task15/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Первый стрелок сбил: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Второй стрелок сбил: ");
+int b = int.Parse(ReadLine()!);
+WriteLine("1 банку они сбили вместе");
+WriteLine($"Всего было {a+b-1} банок");
