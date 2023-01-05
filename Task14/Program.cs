a9033efda1cbe21addbeb841869c739be7245fab
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-05 23:28:16 +0300

    Решил

---
+++ Task14/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+WriteLine(a==b*b?$"{a} является квадаратом {b}":b==a*a?$"{b} является квадратом {a}":"Ни одно из чисел не является корнем другого!");
