2710fa9e6a84b86d31dae142906e80e1602bce52
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-05 23:14:45 +0300

    Решил

---
+++ Task13/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine($"{a}");
+WriteLine(a%7==0 && a%23==0?$"Число {a} кратно 7 и 23!":$"Число {a} не кратно 7 или 23!");
