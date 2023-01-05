4fdbe515a135c0960a2f9c8bfb31eb60d7cf06e5
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-05 22:40:12 +0300

    Решил 2 задачи вписал в 1

---
+++ Task12/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+WriteLine($"{a}, {b}");
+// WriteLine(b%a==0?"Число b кратно числу a.":b%a);
+WriteLine(a==b?"=":a<b?"<":">");
