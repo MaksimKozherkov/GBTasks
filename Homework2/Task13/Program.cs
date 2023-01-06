d4d92a9f2f11b983ead9f2cc29a13350d079d155
MaksimKozherkov <Mirex5915@gmail.com>
2023-01-06 18:06:18 +0300

    Решил

---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число:");
+int number = int.Parse(ReadLine()!);
+
+WriteLine(number>99?(number%1000)/100:"Третьей цифры в заданном числе нет");
