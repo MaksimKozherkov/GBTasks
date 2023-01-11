1f3954d1732c61a552f4a2ec24d40c1cf3073cba
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-12 00:35:47 +0300

    Решили задачу

---
+++ Task21/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число N: ");
+bool isParse = int.TryParse(ReadLine(),out int n);
+int i = 1;
+if(!isParse)
+{
+    WriteLine("Введено не число!");
+    return;
+}
+// Write($"Квадраты чисел от 1 до N: ");
+// while (n <= a)
+// {
+//     Write($"{n*n} ");
+//     // Write($"{n*n} ");
+//     n = n + 1;
+// }
+while(i < n + 1)
+{
+    WriteLine($"Квадрат числа {i} равен {Math.Pow(i,2)}");
+    i++;
+}
