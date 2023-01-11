aff967729576e1c62b1b8ddc627ca731d9f9f92a
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-11 22:29:01 +0300

    Решили задачу

---
+++ Task19/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Всего сделано деталей: ");
+int a = int.Parse(ReadLine()!);
+WriteLine(a%4==0?$"Каждый из мл.токарей сделал {a/4} деталей,а старший токарь сделал {a/2} деталей":$"Токарь ошибся");
