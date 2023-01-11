a97f8eb24a10be46d650680baa7210ceff856ee2
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-11 22:19:04 +0300

    Решили задачу

---
+++ Task18/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите x:");
+int x = int.Parse(ReadLine()!);
+Write("Введите y:");
+int y = int.Parse(ReadLine()!);
+
+if (x==0 || y==0)
+ {
+     WriteLine("Ошибка");
+    return;
+ }
+
+ if (x > 0 && y > 0)
+ {
+     WriteLine("Первая четверть");
+ }
+ if (x < 0 && y > 0)
+ {
+     WriteLine("Вторая четверть");
+ }
+ if (x < 0 && y < 0)
+ {
+     WriteLine("Третья четверть");
+ }
+ if (x > 0 && y < 0)
+ {
+     WriteLine("Четвертая четверть");
+ }
