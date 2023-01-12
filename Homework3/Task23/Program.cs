b197e88ea2ae6d92f5c78416b691a48eb9f9e626
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-12 10:48:18 +0300

    Решил задачу

---
+++ Homework3/Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число N:");
+bool isParse = int.TryParse(ReadLine(),out int n);
+int i = 1;
+if (!isParse) { WriteLine("Введено не число!"); return; }
+
+while (i < n + 1) { WriteLine($"Куб числа {i} равен {Math.Pow(i, 3)}"); i++; }
