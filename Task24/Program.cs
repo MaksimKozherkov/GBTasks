0d2e3d0ea007145a6236cd873e4504d3ba7e3f92
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-31 21:31:49 +0300

    Решил задачу

---
+++ Task24/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+
+
+
+WriteLine($"Колличество цифр в числе {number} равна {NumProduct(number)}");
+
+
+
+
+
+
+int NumProduct(int num)
+{
+    int result = 0;
+    while(num>0)
+    {
+        result++;
+        num/=10;
+    }
+
+    return result;
+}
