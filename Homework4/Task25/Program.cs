f9a1cfca05e26c456d99a1eb9de456a54058d981
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-01 00:10:24 +0300

    Решил задачу

---
+++ Homework4/Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите первое число: ");
+bool numb1 = int.TryParse(ReadLine(),out int number1);
+WriteLine("Введите второе число: ");
+bool numb2 = int.TryParse(ReadLine(),out int number2);
+
+
+
+
+WriteLine($"{NamProduct(number1,number2)}");
+
+
+
+
+
+
+int NamProduct(int num1, int num2)
+{
+    int i = 1;
+    int num3=num1;
+    while(i<num2)
+    {
+        num1*=num3;
+        i++;
+    }
+
+    return num1;
+}
