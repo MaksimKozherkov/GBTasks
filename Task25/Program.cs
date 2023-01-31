5a54d94df064ece9643ea8f3b4b589f333be9d9a
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-31 23:01:51 +0300

    Решил задачу

---
+++ Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+Clear();
+
+int[] array = InitBinaryArray();
+PrintArray(array);
+
+
+int[] InitBinaryArray()
+{
+    int[] res=new int[8];
+    for (int i = 0; i < res.Length; i++)
+    {
+        Random  ran = new Random();
+        res[i] = ran.Next(2);
+    }
+    return res;
+}
+
+
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Write($"{arr[i]} ");    
+    }
+    WriteLine();
+}
