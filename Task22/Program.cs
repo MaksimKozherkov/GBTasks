a8699f1c4ee0210f5e3bf0bb6c3fe24d8d7229c2
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-31 20:37:30 +0300

    Решил задачу

---
+++ Task22/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Input number1: ");
+int number1 = int.Parse(ReadLine()!);
+
+int sum1 = GetSum(number1);
+WriteLine($"Sum namber 1..A = {sum1}");
+
+WriteLine("Input number2: ");
+int number2 = int.Parse(ReadLine()!);
+
+int sum2 = GetSum(number2);
+WriteLine($"Sum namber 1..A = {sum2}");
+
+
+
+
+
+
+int GetSum(int A)
+{
+    int result = 0;
+    while (A > 0)
+    {
+        result += A;
+        A--;
+    }
+
+    return result;
+}
