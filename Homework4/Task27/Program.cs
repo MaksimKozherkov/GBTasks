7b779e289aa5b11262ef33d58da55db0353b98ab
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-31 23:30:46 +0300

    Решил задачу

---
+++ Homework4/Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+bool numb = int.TryParse(ReadLine(),out int number);
+
+
+
+
+WriteLine($"Сумма цифр в числе {number} равна {NumSumm(number)}");
+
+
+
+
+
+
+int NumSumm(int num)
+{
+    int result = 0;
+    int a=0;
+    while(num>0)
+    {
+        a=num%10;
+        num/=10;
+        result+=a;
+    }
+
+    return result;
+}
