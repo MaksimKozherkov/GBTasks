104ac9203662dcbc95efc4338f4b11e7cf29127e
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-31 20:59:01 +0300

    Решил задачу

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int work = GetWork(number);
+WriteLine($"Произведение чисел от 1 до {number} равно {work}.");
+
+
+
+
+
+
+
+
+
+int GetWork(int A)
+{
+    int result = 1;
+    while (A>0)
+    {
+        result *= A;
+        A--;
+    }
+    return result;
+}
