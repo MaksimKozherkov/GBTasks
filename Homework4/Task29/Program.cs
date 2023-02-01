8ca357024f3f97cf570976dffab330c3e7354510
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-01 23:51:46 +0300

    Решил задачу

---
+++ Homework4/Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+Clear();
+
+int[] array = InitArray();
+
+
+
+int[] InitArray()
+{
+    int[] res=new int[8];
+    for (int i = 0; i < res.Length; i++)
+    {
+        Random  ran = new Random();
+        res[i] = ran.Next(100);
+        Write("{0} ", res[i]);
+        
+    }
+    return res;
+    
+    
+}
+
+
+
