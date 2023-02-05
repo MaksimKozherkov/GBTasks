14ff7e4a452765a522af5aa033cef590bb19b6bc
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-05 23:09:19 +0300

    Решил задачу

---
+++ Task27/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+Clear();
+
+
+int[] array = { -1, -2, 0, 2, 1, 3, 4 };
+
+
+InverseArray(array);
+PrintArray(array);
+
+
+
+
+
+void InverseArray(int[] InArray)
+{
+    for (int i = 0; i < InArray.Length; i++)
+    {
+        InArray[i] *= -1;
+    }
+}
+
+void PrintArray(int[] InArray)
+{
+    Write("[");
+    for (int i = 0; i < InArray.Length - 1; i++)
+    {
+        Write($"{InArray[i]},");
+    }
+    WriteLine($"{InArray[InArray.Length - 1]}]");
+}
+
+
