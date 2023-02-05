ddc58228fd7277ec7483c4c5398170c2fa0bb0f4
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-06 01:24:56 +0300

    Решил задачу

---
+++ Homework5/Task34/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using static System.Console;
+
+Clear();
+
+
+
+
+
+int[] array = GetArray(10);
+
+int countInArray = EvenNumberInArray(array);
+printArray(array);
+
+Write($"Количество четных элементов в массиве: равно {countInArray}");
+
+
+
+int[] GetArray(int size)
+{
+    int[] resultArray = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = random.Next(99, 1000);
+    }
+
+    return resultArray;
+}
+
+int EvenNumberInArray(int[] array)
+{
+    int result = 0;
+    foreach (int i in array)
+    {
+        if (i%2==0) result++;
+    }
+    return result;
+}
+
+void printArray(int[] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Write($"{array[i]}, ");
+    }
+
+    WriteLine($"{array[array.Length - 1]}]");
+}
+
+
+
+
+
+
+
+
