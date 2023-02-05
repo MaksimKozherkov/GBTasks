d3f10fd8b2a14261f7c7608c2db1afcbead2d985
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-05 23:34:52 +0300

    Решил задачу

---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int.TryParse(ReadLine(),out int number);
+int[] array = { -1, -2, 0, 2, 1, 3, 4 };
+
+
+bool result = ArrayLookUp(array,number);
+WriteLine(result);
+
+
+
+
+bool ArrayLookUp(int[] InArray,int num)
+{
+    int result=0;
+    foreach (var a in array)
+    {
+        if(num==a) return true;
+    }
+    return false;
+}
+
+
+
