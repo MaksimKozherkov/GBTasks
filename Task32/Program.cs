6632571c5b397e464805a3a85691daa7194feea0
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-08 22:48:48 +0300

    Решил задачу

---
+++ Task32/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+Clear();
+
+int a= AskUser("Введите сторону треугольника a: ");
+int b= AskUser("Введите сторону треугольника b: ");
+int c= AskUser("Введите сторону треугольника c: ");
+
+WriteLine(IsTriangle(a,b,c)?$"Треугольник со сторонами {a}, {b} и {c} существует.":$"Треугольник со сторонами {a}, {b} и {c} не существует.");
+
+bool IsTriangle(int InA,int InB,int inC)//проверяет введенные данные на заданные условия
+{
+    if(a+b>c&&a+c>b&&b+c>a)return true;
+    return false;
+}
+
+
+int AskUser(string inAsk)//задает пользователю вопрос и считывает числовой ответ
+{
+    int resultAsk=0;
+    string userAsk="";
+    while(!int.TryParse(userAsk,out resultAsk))
+    {
+        Console.Write(inAsk);
+        userAsk=Console.ReadLine()!;
+    }
+    return resultAsk;
+}
+
+
+
+
