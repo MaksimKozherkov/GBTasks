cbe9dc5adf32fa1bef3c10e09e33a2b98f1e5a8d
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-12 21:52:32 +0300

    Решил задачу

---
+++ Task36/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите размер матрицы через пробел: ");
+string[] parameters=ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array=GetMatrixArray(int.Parse(parameters[0]),int.Parse(parameters[1]));
+PrintMatrixArray(array);
+
+
+int[,] GetMatrixArray(int rows,int colums)
+{
+    int[,] resultArray=new int[rows,colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            resultArray[i,j]=i+j;
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j],5} ");
+        }
+        WriteLine();
+    }
+}
