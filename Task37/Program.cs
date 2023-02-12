6fedc4ae3956604954f20c3f13febae79a87b645
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-12 22:24:12 +0300

    Решил задачу

---
+++ Task37/Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите размер матрицы и диапазон значений через пробел: ");
+string[] parameters=ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array=GetMatrixArray(int.Parse(parameters[0]),int.Parse(parameters[1]),int.Parse(parameters[2]),int.Parse(parameters[3]));
+PrintMatrixArray(array);
+WriteLine();
+int[,] matrix=newMatrix(array);
+PrintMatrixArray(matrix);
+
+int[,] GetMatrixArray(int rows,int colums, int minValue, int maxValue)
+{
+    Random rnd=new Random();
+    int[,] resultArray=new int[rows,colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            resultArray[i,j]=rnd.Next(minValue,maxValue+1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j],5} ");
+        }
+        WriteLine();
+    }
+}
+
+
+int[,] newMatrix(int[,] inArray)
+{
+     for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if(i%2==0&&j%2==0&&i>0&&j>0)
+            {
+                inArray[i,j]=inArray[i,j]*inArray[i,j];
+            }
+        }
+    }
+    return inArray;
+}
