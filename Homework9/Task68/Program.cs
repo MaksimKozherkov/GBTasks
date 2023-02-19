13ba489bb142fc5c4d52a98ef896f544cf2bbc7f
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-19 21:10:04 +0300

    Решил задачу

---
+++ Homework9/Task68/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+Clear();
+
+
+Write("Введите два неотрицательных числа, через пробел: ");
+int[] numbers = Array.ConvertAll(ReadLine()!.Split(new string[] { " ", ", ", ";" }, StringSplitOptions.RemoveEmptyEntries), Convert.ToInt32);
+if (numbers[0] < 0 || numbers[1] < 0)
+{
+    Write("Ошибка! Введите неотрицательные числа!");
+    return;
+}
+int result = GetNumbers(numbers[0], numbers[1]);
+
+WriteLine();
+WriteLine("Результат:");
+WriteLine($"m = {numbers[0]}, n = {numbers[1]} -> A(m, n) = {result}");
+WriteLine();
+
+int GetNumbers(int M, int N)
+{
+
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (M > 0 && N == 0)
+    {
+        return GetNumbers(M - 1, 1);
+    }
+    else
+    {
+        return GetNumbers(M - 1, GetNumbers(M, N - 1));
+    }
+}
