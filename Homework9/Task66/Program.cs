9092c43073df838540406e340c9b36a5f6e7ae40
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-19 21:06:11 +0300

    Решил задачу

---
+++ Homework9/Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+Clear();
+
+
+Write("Введите первое и второе число через пробел: ");
+string[] par = ReadLine()!.Split(new string[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries);
+int M = int.Parse(par[0]);
+int N = int.Parse(par[1]);
+
+WriteLine();
+WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}:");
+WriteLine();
+Write($"M = {M}; N = {N} -> ");
+WriteLine(SumNumbers(M, N));
+WriteLine();
+
+int SumNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        int temp = numberM;
+        numberM = numberN;
+        numberN = temp;
+    }
+    if (numberM == numberN) return numberM;
+    else
+    {
+        return SumNumbers(numberM + 1, numberN) + numberM;
+    }
+}
